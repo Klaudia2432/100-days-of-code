5165afa895e4791f7e2b1bdfd3824407605b6c3c
Klaudia <kdziubek@happcode.com>
2023-02-02 21:19:02 +0000

    Added code from day 85

--- Project-an-online-shop/app.js
+++ Project-an-online-shop/app.js
@@ -19,6 +19,7 @@ const sessionConfig = createSessionConfig();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
+app.use('/products/assets', express.static('product-data'));
 app.use(express.urlencoded({ extended: false }));
 app.use(expressSession(sessionConfig));
 app.use(csrf());
--- Project-an-online-shop/routes/admin.routes.js
+++ Project-an-online-shop/routes/admin.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const adminController = require('../controllers/admin.controller');
+const imageUpload = require('../middlewares/image-upload');
 const router = express.Router();
 
 router.get('/products', adminController.getProducts);
 
 router.get('/products/new', adminController.getNewProduct);
+
+router.post('/products', imageUpload, adminController.createNewProduct)
 
 module.exports = router;
