9444ccc30baa4f05635d85f855a0512f6e77227d
Klaudia <kdziubek@happcode.com>
2023-02-06 10:10:59 +0000

    Added code from day 89

--- Project-an-online-shop/app.js
+++ Project-an-online-shop/app.js
@@ -5,6 +5,7 @@ const expressSession = require("express-session");
 const createSessionConfig = require("./config/session");
 const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const protectedRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require('./middlewares/cart');
 
 const authRoutes = require("./routes/auth.routes");
 const productRoutes = require('./routes/products.routes');
@@ -16,20 +17,25 @@ const db = require("./data/database");
 const addCSRFToken = require("./middlewares/csrf-token");
 const errorHandler = require("./middlewares/error-handler");
 const sessionConfig = createSessionConfig();
+const cartRoutes = require('./routes/cart.routes')
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use('/products/assets', express.static('product-data'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use(expressSession(sessionConfig));
 app.use(csrf());
 app.use(addCSRFToken);
+app.use(cartMiddleware);
 app.use(checkAuthStatusMiddleware);
 
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productRoutes);
+app.use('/cart', cartRoutes);
 app.use(protectedRoutesMiddleware);
 app.use('/admin', adminRoutes);
 
